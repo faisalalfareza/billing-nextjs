4e7c15858d5fb3b7fb67805e923a83c3e071c55e
nanda1211 <nanda.destyandana@visionet.co.id>
2023-04-12 10:58:09 +0700

    cek

--- pages/transaction/oracle-to-journal/index.js
+++ pages/transaction/oracle-to-journal/index.js
@@ -1,19 +1,33 @@
 import Card from "@mui/material/Card";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
 // @mui material components
-import { Grid } from "@mui/material";
+import { Grid, TextField } from "@mui/material";
 // NextJS Material Dashboard 2 PRO components
 import MDBox from "/components/MDBox";
 import MDTypography from "/components/MDTypography";
+import Icon from "@mui/material/Icon";
 
 // NextJS Material Dashboard 2 PRO layout
 import DashboardLayout from "/layout/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "/layout/Navbars/DashboardNavbar";
+import Footer from "/layout/Footer";
+import DataTable from "/layout/Tables/DataTable";
+import MDButton from "/components/MDButton";
+import MDInput from "/components/MDInput";
+import FormField from "/pagesComponents/FormField";
+import Autocomplete from "@mui/material/Autocomplete";
 import { useMaterialUIController } from "/context";
+import { useCookies } from "react-cookie";
 import * as React from "react";
+import { typeNormalization, downloadTempFile } from "/helpers/utils";
+import { alertService } from "/helpers";
 
 // Data
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
 import SiteDropdown from "../../../pagesComponents/dropdown/Site";
+import PeriodDropdown from "../../../pagesComponents/dropdown/Period";
 // import templateWaterReading from "../assets/template/template-water-reading.xlsx";
 
 export default function OracleToJournal(props){
@@ -24,29 +38,101 @@ export default function OracleToJournal(props){
       totalPages: undefined,
     });
 
-    const form = {
-        formId: "oracle-to-journal",
-        formField: {
-            period: {
-                name: "period",
-                label: "period",
-                placeholder: "Type Period",
-                type: "text",
-                isRequired: true,
-                errorMsg: "Period is required",
-                defaultValue: ""
-            }
+    const [dataPeriod, setDataPeriod] = useState([]);
+    const [site, setSite] = useState(null);
+    const [{ accessToken, encryptedAccessToken }] = useCookies();
+
+    useEffect(() => {
+        let currentSite = JSON.parse(localStorage.getItem("site"));
+        console.log("currentSite-----------", currentSite);
+        if (currentSite == null) {
+        alertService.info({
+            title: "Info!",
+            text: "Please choose Site first",
+        });
+        } else {
+        setSite(currentSite);
         }
+        getPeriod();
+    }, []);
+
+    const [customerRequest, setCustomerRequest] = useState({
+            scheme: site?.siteId,
+            keywords: "",
+            recordsPerPage: 10,
+            skipCount: 0,
+        });
+
+        const skipCountChangeHandler = (e) => {
+        customerRequest.skipCount = e;
+        setCustomerRequest((prevState) => ({
+        ...prevState,
+        skipCount: e,
+        }));
+    };
+    const recordsPerPageChangeHandler = (e) => {
+        customerRequest.recordsPerPage = e;
+        setCustomerRequest({
+        ...prevState,
+        recordsPerPage: e,
+        });
+    };
+    const keywordsChangeHandler = (e) => {
+        customerRequest.keywords = e;
+        setCustomerRequest((prevState) => ({
+        ...prevState,
+        keywords: e,
+        }));
     };
 
-    const { period } = form.formField;
-
-    const [site, setSite] = useState(null);
+    const getPeriod = async (val) => {
+        let response = await fetch("/api/transaction/oracle-to-journal/dropdownperiod", {
+        method: "POST",
+        body: JSON.stringify({
+            accessToken: accessToken,
+            params: {
+            SiteId: site?.siteId,
+            },
+        }),
+        });
+        if (!response.ok) throw new Error(`Error: ${response.status}`);
+        response = typeNormalization(await response.json());
+        console.log("response----", response);
+        if (response.error) {
+        alertService.error({ title: "Error", text: response.error.message });
+        } else {
+        setDataPeriod(response.result);
+        }
+        console.log("project------", dataPeriod);
+    };
+    useEffect(() => {
+        getPeriod();
+    }, [site]);
+    useEffect(() => {
+        //fetchData();
+    }, [customerRequest.skipCount, customerRequest.recordsPerPage]);
 
     const handleSite = (siteVal) => {
         setSite(siteVal);
         localStorage.setItem("site", JSON.stringify(siteVal));
     };
+
+    const form = {
+        formId: "oracle-to-journal",
+        formField: {
+            period: {
+                name: "period",
+                label: "Period",
+                placeholder: "Type Period",
+                type: "text",
+                isRequired: true,
+                errorMsg: "Period is required.",
+                defaultValue: "",
+            }
+        }
+    }
+
+    const { period } = form.formField;
 
     return (
         <DashboardLayout>
@@ -88,39 +174,49 @@ export default function OracleToJournal(props){
                                         <Grid item xs={12} md={6}>
                                             <Autocomplete
                                                 // disableCloseOnSelect
-                                                key={cluster.name}
-                                                options={dataCluster}
-                                                value={values.cluster}
+                                                // includeInputInList={true}
+                                                options={dataPeriod}
+                                                key={period.name}
+                                                value={values.project}
+                                                // getOptionSelected={(option, value) => {
+                                                //   return (
+                                                //     option.projectName === value.projectName
+                                                //   );
+                                                // }}
                                                 getOptionLabel={(option) =>
-                                                option.clusterCode +
-                                                " - " +
-                                                option.clusterName
+                                                values.project != {}
+                                                    ? option.periodId +
+                                                    " - " +
+                                                    option.periodName
+                                                    : "Nothing selected"
                                                 }
-                                                onChange={(e, value) =>
+                                                onChange={(e, value) => {
                                                 setFieldValue(
-                                                    cluster.name,
+                                                    project.name,
                                                     value !== null
                                                     ? value
-                                                    : initialValues[cluster.name]
-                                                )
-                                                }
+                                                    : initialValues[period.name]
+                                                );
+                                                onProjectChange(value);
+                                                }}
+                                                noOptionsText="No results"
                                                 renderInput={(params) => (
                                                 <FormField
                                                     {...params}
-                                                    type={cluster.type}
+                                                    type={period.type}
                                                     label={
-                                                    cluster.label +
-                                                    (cluster.isRequired ? " *" : "")
+                                                    period.label +
+                                                    (period.isRequired ? " *" : "")
                                                     }
-                                                    name={cluster.name}
-                                                    placeholder={cluster.placeholder}
+                                                    name={period.name}
+                                                    placeholder={period.placeholder}
                                                     InputLabelProps={{ shrink: true }}
                                                     error={
-                                                    errors.cluster && touched.cluster
+                                                    errors.period && touched.period
                                                     }
                                                     success={checkingSuccessInput(
-                                                    cluster,
-                                                    errors.cluster
+                                                    period,
+                                                    errors.period
                                                     )}
                                                 />
                                                 )}
