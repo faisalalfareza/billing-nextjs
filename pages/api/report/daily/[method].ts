aef575b938fad3399e83c0c55d123d5d1be4ed47
shyntaayu <shynta.add@gmail.com>
2023-04-18 10:15:42 +0700

    + report daily ui

---
+++ pages/api/report/daily/[method].ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import getConfig from "next/config";
+import axios from "axios";
+
+const { publicRuntimeConfig } = getConfig();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let { method, query, body } = req;
+  body = JSON.parse(body);
+  switch (method) {
+    case "POST":
+      switch (query.method) {
+        case "reportdaily":
+          getReport(res, body);
+          break;
+      }
+      break;
+  }
+}
+
+async function getReport(res: any, body: any) {
+  const { accessToken, params } = body;
+
+  const url = `${publicRuntimeConfig.apiUrl}/api/services/app/Report/ReportDaily`;
+  const config = {
+    headers: {
+      Authorization: "Bearer " + accessToken,
+      "Access-Control-Allow-Origin": "*",
+    },
+    params: params,
+  };
+
+  axios
+    .post(url, params, config)
+    .then((response) =>
+      res.send({
+        result: response.data.result,
+      })
+    )
+    .catch((error) =>
+      res.send({
+        error: error,
+      })
+    );
+}
