f50637d158a28b0b0fa7986d665edd8c0fdedb4d
Faisal Alfareza <alfarezafaisal12@gmail.com>
2023-08-24 10:50:08 +0700

    .

--- middleware.ts
+++ middleware.ts
@@ -5,14 +5,6 @@ import { typeNormalization } from "./helpers/utils";
 let alreadyLoggedIn: boolean = false;
 export const config = {
   matcher: [
-    /*
-        Match all request paths except for the ones starting with:
-        - api (API routes)
-        - _next/static (static files)
-        - _next/image (image optimization files)
-        - favicon.ico (favicon file)
-     */
-    // "/((?!api|_next/static|_next/image|favicon.ico).*)",
     "/((?!api|_next/static|_next/image|template|favicon.ico).*)",
   ],
 };
